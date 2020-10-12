c008249e27766e9731da5721480f0a5a94fd26dd
seoe5580 <seoe5580@wrdsb.ca>
2020-10-12 22:37:23 +0000

    Done number finding question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Finds a specific number from the numbers that the user put
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to input 10 different integers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    // sets the amount of array
+    int[] num = new int[10];
     
+    for(int i = 0; i < num.length; i++){
+      num[i] = input.nextInt();
+    }
+
+    System.out.println("Please enter a number to find");
+
+    int sameNum = input.nextInt();
+
+    for(int i = 0; i < num.length; i++){
+      if(num[i] == sameNum){
+        System.out.println(sameNum + " is located at index " + (i + 1));
+      }
+    }
   }
 }
